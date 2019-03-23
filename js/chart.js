517a3c7999fcbe58320013f1a0eb4f10b7088d8a
Madeline Watson <mcwatson91@gmail.com>
2019-03-23 16:48:33 -0400

    bar chart

--- js/chart.js
+++ js/chart.js
@@ -1,3 +1,4 @@
+// create line chart
 const trafficChart = document.getElementById('trafficLineChart');
 
 var lineChart = new Chart(trafficChart, {
@@ -6,12 +7,18 @@ var lineChart = new Chart(trafficChart, {
     labels: ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
     datasets: [
       {
-        label: '2018',
-        data: [804, 1038, 1019, 1488, 929, 1371, 1526, 1578, 1832, 2115, 1993, 2002]
+        // label: false,
+        data: [804, 1038, 1019, 1488, 929, 1371, 1426, 1578, 1532, 1215, 1493, 1516]
       }
     ]
   },
   options: {
+    title: {
+      display: false
+    },
+    legend: {
+      display: false
+      },
     layout: {
       padding: {
         left: 30,
@@ -19,6 +26,29 @@ var lineChart = new Chart(trafficChart, {
         top: 10,
         bottom: 10
       }
+    }
+  }
+});
+
+// create bar chart
+const trafficBarChart = document.getElementById('trafficBarChart');
+
+var barChart = new Chart(trafficBarChart, {
+  type: 'bar',
+  data: {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [
+      {
+        data: [75, 100, 148, 109, 196, 189, 77],
+        backgroundColor: [
+          '#5840A0', '#5840A0', '#5840A0', '#5840A0', '#5840A0', '#5840A0', '#5840A0'
+        ]
+      }
+    ]
+  },
+  options: {
+    legend: {
+      display: false
     }
   }
 });
