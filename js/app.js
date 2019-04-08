2a706a7869e3de129f6464c63ef7c0161d746176
Madeline Watson <mcwatson91@gmail.com>
2019-04-08 18:02:05 -0400

    send button js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 // close the alert bar
 
 const alertBar = document.querySelector('.alert');
-const close = document.getElementById('close');
+const closeAlert = document.getElementById('alert_close');
 
-close.addEventListener('click', () =>{
+closeAlert.addEventListener('click', () =>{
   alertBar.style.visibility = 'hidden';
+});
+
+// send message button
+
+const send = document.getElementById('send_btn');
+const hiddenMessage = document.querySelector('.hidden');
+const closeMsg = document.getElementById('msg_close');
+const search = document.getElementById('user_search');
+const message = document.getElementById('user_message');
+
+send.addEventListener('click', (e) => {
+
+  if (search.value == '' || message.value == '') {
+    console.log("NO");
+  } else {
+  // do this if form is filled out correctly
+  hiddenMessage.classList.remove('hidden');
+  hiddenMessage.classList.add('show');
+  e.preventDefault();
+  }
+});
+
+closeMsg.addEventListener('click', () => {
+  hiddenMessage.classList.remove('show');
+  hiddenMessage.classList.add('hidden');
 })
