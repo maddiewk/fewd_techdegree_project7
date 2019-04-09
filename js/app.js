39382b8e88fb1403763d5acfe34c597f6c465112
Madeline Watson <mcwatson91@gmail.com>
2019-04-09 15:23:05 -0400

    message functionality

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 // close the alert bar
 
-const alertBar = document.querySelector('.alert');
-const closeAlert = document.getElementById('alert_close');
+// const alertBar = document.querySelector('.alert');
+// const closeAlert = document.getElementById('alert_close');
 
-closeAlert.addEventListener('click', () =>{
-  alertBar.style.visibility = 'hidden';
+const $alertBar = $('.alert');
+const $closeAlert = $('#alert_close');
+const $send = $('#send_btn');
+const $hiddenMessage = $('.hidden');
+const $closeMsg = $('#msg_close');
+const $search = $('#user_search');
+const $message = $('#user_message');
+const $parentDiv = $('.message');
+const $appendDiv = $('#dynamic');
+const $errorMsgClose = $('#msg_close');
+
+$closeAlert.on('click', () => {
+  $alertBar.slideUp(800);
 });
 
-// send message button
-
-const send = document.getElementById('send_btn');
-const hiddenMessage = document.querySelector('.hidden');
-const errorMessage = document.querySelector('.hidden_two');
-const closeMsg = document.getElementById('msg_close');
-const search = document.getElementById('user_search');
-const message = document.getElementById('user_message');
-
-send.addEventListener('click', (e) => {
-
-  if (search.value === '' || message.value === '') {
-    errorMessage.classList.remove('hidden_two');
-    errorMessage.classList.add('error_msg');
-    console.log("Please select a user.");
-  } else {
-    // do this if form is filled out correctly
-    hiddenMessage.classList.remove('hidden');
-    hiddenMessage.classList.add('show');
-    console.log("Great! You filled out all the required fields.");
-  }
+$send.on('click', (e) => {
   e.preventDefault();
+  $appendDiv.css('display', 'block');
+  $appendDiv.html('');
+  if ($search.val() === '' || $message.val() === '') {
+    $appendDiv.delay(3000).append(`<span>! Please make sure both form fields are correctly filled in.</span>`).slideDown(1500).removeClass('hidden').hide(1000);
+  } else {
+    $appendDiv.css('display', 'none');
+    $hiddenMessage.addClass('show').removeClass('hidden');
+    $search.val('');
+    $message.val('');
+  }
 });
 
-closeMsg.addEventListener('click', () => {
-  hiddenMessage.classList.remove('show');
-  hiddenMessage.classList.add('hidden');
-})
+$errorMsgClose.on('click', () => {
+  $hiddenMessage.addClass('hidden').removeClass('show');
+});
